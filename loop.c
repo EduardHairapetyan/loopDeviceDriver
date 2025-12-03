8b9301ad804840829c6a2dabe1357c83b9ca2281
Ed <a.eduard.hayrapetyan@gmail.com>
2025-12-03 18:38:22 +0400

    fixed memory leaks

--- loop.c
+++ loop.c
@@ -20,7 +20,6 @@ typedef struct FileContext {
     struct file * file;
     loff_t g_koffset;
     loff_t g_uoffset;
-    // uint8_t  kbuf[16];              // one 16-byte chunk
     uint16_t prev_line[8];
     bool prev_identical;
     bool first_line;
@@ -35,7 +34,6 @@ static FileContext file_ctx = {
     .file = NULL,
     .g_koffset = 0,
     .g_uoffset = 0,
-    // .kbuf = {0},
     .prev_line = {0},
     .prev_identical = false,
     .first_line = true,
@@ -93,7 +91,6 @@ static int dev_release(struct inode *inode, struct file *file)
     // Reset context 
     file_ctx.g_koffset = 0;
     file_ctx.g_uoffset = 0;
-    // memset(file_ctx.kbuf,0,sizeof(file_ctx.kbuf));
     memset(file_ctx.prev_line,0,sizeof(file_ctx.prev_line));
     file_ctx.prev_identical = false;
     file_ctx.first_line = true;
@@ -122,8 +119,7 @@ static ssize_t dev_write(struct file *file, const char __user *buf,
     size_t total_written = 0;
     size_t hex_offset = file_ctx.g_koffset;
 
-    char linebuf[64];       // final formatted output per line
-
+    // Read from user buffer in chunks
     while (total_written < len) {
 
         size_t chunkSize = min(len - total_written, MAX_CHUNK_SIZE);
@@ -131,73 +127,79 @@ static ssize_t dev_write(struct file *file, const char __user *buf,
         uint8_t* chunk = kmalloc(chunkSize, GFP_KERNEL);
 
         if (copy_from_user(chunk, buf + total_written, chunkSize))
+        {
+            kfree(chunk);
             return total_written ? total_written : -EFAULT;
-
+        }
+        
         // now from small buffer read 16 byte chunks
         uint8_t kbuf[16] = {0};
 
+        // process 16-byte chunks
         for (size_t i = 0; i < chunkSize; i += 16) {
             uint8_t curr_chunk_size = min((size_t)16, chunkSize - i);
             memcpy(kbuf, chunk + i, curr_chunk_size);
+            char linebuf[64];       // final formatted output per line
 
-        // parse bytes → uint16_t words
-        uint16_t curr_line[8] = {0};
-        for (size_t i = 0; i < curr_chunk_size; i += 2) {
-            curr_line[i/2] = kbuf[i] | ((i+1 < curr_chunk_size ? kbuf[i+1] : 0) << 8);
-        }
-
-        // detect identical repeated line
-        if (!file_ctx.first_line && memcmp(curr_line, file_ctx.prev_line, sizeof(curr_line)) == 0) {
-
-            if (!file_ctx.prev_identical) {
-                // first time we see repeated line → output "*"
-                kernel_write(file_ctx.file, "*\n", 2, offset);
-                file_ctx.prev_identical = true;
+            // parse bytes → uint16_t words
+            uint16_t curr_line[8] = {0};
+            for (size_t i = 0; i < curr_chunk_size; i += 2) {
+                curr_line[i/2] = kbuf[i] | ((i+1 < curr_chunk_size ? kbuf[i+1] : 0) << 8);
             }
-        } else {
 
-            // build line: "00001fc0 1234 5678 ...\n"
-            int pos = 0;
+            // detect identical repeated line
+            if (!file_ctx.first_line && memcmp(curr_line, file_ctx.prev_line, sizeof(curr_line)) == 0) {
 
-            // offset 7 digits hex padded
-            pos += scnprintf(linebuf + pos, sizeof(linebuf) - pos, "%07zx ", hex_offset);
+                if (!file_ctx.prev_identical) {
+                    // first time we see repeated line → output "*"
+                    kernel_write(file_ctx.file, "*\n", 2, offset);
+                    file_ctx.prev_identical = true;
+                }
+            } else {
 
-            // words
-            for (int i = 0; i < 8; i++) {
-                if (i < curr_chunk_size/2) {
-                    hex16(linebuf + pos, curr_line[i]);
-                    pos += 4;
-                } else {
+                // build line: "00001fc0 1234 5678 ...\n"
+                int pos = 0;
 
-                    
-                    if( i != curr_chunk_size/2)
+                // offset 7 digits hex padded
+                pos += scnprintf(linebuf + pos, sizeof(linebuf) - pos, "%07zx ", hex_offset);
+
+                // words
+                for (int i = 0; i < 8; i++) {
+                    if (i < curr_chunk_size/2) {
+                        hex16(linebuf + pos, curr_line[i]);
+                        pos += 4;
+                    } else {
+
+                        
+                        if( i != curr_chunk_size/2)
+                            linebuf[pos++] = ' ';
+                        
                         linebuf[pos++] = ' ';
-                    
-                    linebuf[pos++] = ' ';
-                    linebuf[pos++] = ' ';
-                    linebuf[pos++] = ' ';
-                    linebuf[pos++] = ' ';
-                    continue;
+                        linebuf[pos++] = ' ';
+                        linebuf[pos++] = ' ';
+                        linebuf[pos++] = ' ';
+                        continue;
+                    }
+
+                    if (i < 7)
+                        linebuf[pos++] = ' ';
                 }
 
-                if (i < 7)
-                    linebuf[pos++] = ' ';
+                linebuf[pos++] = '\n';
+
+                kernel_write(file_ctx.file, linebuf, pos, offset);
+
+                memcpy(file_ctx.prev_line, curr_line, sizeof(curr_line));
+                file_ctx.prev_identical = false;
             }
+        
 
-            linebuf[pos++] = '\n';
-
-            kernel_write(file_ctx.file, linebuf, pos, offset);
-
-            memcpy(file_ctx.prev_line, curr_line, sizeof(curr_line));
-            file_ctx.prev_identical = false;
+            file_ctx.first_line = false;
+            total_written += curr_chunk_size;
+            hex_offset += curr_chunk_size;
         }
-    
-
-        file_ctx.first_line = false;
-        total_written += curr_chunk_size;
-        hex_offset += curr_chunk_size;
+        kfree(chunk);
     }
-}
 
     file_ctx.g_koffset += len;
     file_ctx.g_uoffset = *offset;
